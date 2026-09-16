64e29289f42775486362604ed2d6aa94d77b9877
Pedro Henrique <pedrosilva1anocim@gmail.com>
2026-09-16 14:31:11 -0300

    fix: mark SocialProof as client component

--- src/components/sections/SocialProof.tsx
+++ src/components/sections/SocialProof.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+"use client";
 import { useEffect, useRef, useState } from "react";
 import { Container } from "@/components/ui/Container";
 import { Reveal } from "@/components/ui/Reveal";
